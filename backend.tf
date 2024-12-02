dc35deff7923a6a325f02307227365482e998cb3
Ahmed Waldra <ahmedwaldara@gmail.com>
2024-12-02 23:25:34 +0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-backend"
-    key    = "terraform/terraform.tfstate"
+    key    = "three-tier/terraform.tfstate"
     region = "eu-west-1"
     dynamodb_table = "terraform-state"
   }
